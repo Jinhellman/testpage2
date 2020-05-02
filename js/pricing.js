6f88ccc1c48b7aaed020d42c872267365e7609a5
Jinhellman <60388001+Jinhellman@users.noreply.github.com>
2020-05-03 01:55:09 +0400

    new

---
+++ js/pricing.js
@@ -0,0 +1,118 @@
+function myFunction1(){
+  document.getElementsByClassName("price-1")[0].innerHTML= "₾ 99";
+  document.getElementsByClassName("price-2")[0].style= "visibility:visible";
+  document.getElementsByClassName("price-2")[0].innerHTML= "0%";
+  document.getElementsByClassName("price-3")[0].style="visibility:visible";
+  document.getElementsByClassName("price-3")[0].innerHTML= "ფასდაკლება";
+  document.getElementsByClassName("minus-price")[0].style= "visibility:hidden";
+  document.getElementsByClassName("minus-none")[0].style="visibility:hidden";
+  document.getElementsByClassName("minus-none")[0].innerHTML= "0000";
+  document.getElementsByClassName("number-icon")[1].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[2].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[3].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[4].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[5].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("infinity-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[0].style="box-shadow: 0px 2px #fff";
+}
+function myFunction2(){
+  document.getElementsByClassName("price-1")[0].innerHTML= "₾ 565";
+  document.getElementsByClassName("price-2")[0].style= "visibility:visible";
+  document.getElementsByClassName("price-2")[0].innerHTML= "-5%";
+  document.getElementsByClassName("price-3")[0].style="visibility:visible";
+  document.getElementsByClassName("price-3")[0].innerHTML= "ფასდაკლება";
+  document.getElementsByClassName("minus-price")[0].style= "visibility:visible";
+  document.getElementsByClassName("minus-price")[0].innerHTML= "₾594";
+  document.getElementsByClassName("minus-none")[0].innerHTML= "₾594";
+  document.getElementsByClassName("number-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[2].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[3].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[4].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[5].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("infinity-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[1].style="box-shadow: 0px 2px #fff";
+}
+function myFunction3(){
+  document.getElementsByClassName("price-1")[0].innerHTML= "₾ 1,069";
+  document.getElementsByClassName("price-2")[0].style= "visibility:visible";
+  document.getElementsByClassName("price-2")[0].innerHTML= "-10%";
+  document.getElementsByClassName("price-3")[0].style="visibility:visible";
+  document.getElementsByClassName("price-3")[0].innerHTML= "ფასდაკლება";
+  document.getElementsByClassName("minus-price")[0].style= "visibility:visible";
+  document.getElementsByClassName("minus-price")[0].innerHTML= "₾1,188";
+  document.getElementsByClassName("minus-none")[0].innerHTML= "₾594";
+  document.getElementsByClassName("number-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[1].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[3].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[4].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[5].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("infinity-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[2].style="box-shadow: 0px 2px #fff";
+}
+function myFunction4(){
+  document.getElementsByClassName("price-1")[0].innerHTML= "₾ 1,900";
+  document.getElementsByClassName("price-2")[0].style= "visibility:visible";
+  document.getElementsByClassName("price-2")[0].innerHTML= "-20%";
+  document.getElementsByClassName("price-3")[0].style="visibility:visible";
+  document.getElementsByClassName("price-3")[0].innerHTML= "ფასდაკლება";
+  document.getElementsByClassName("minus-price")[0].style= "visibility:visible";
+  document.getElementsByClassName("minus-price")[0].innerHTML= "₾2,376";
+  document.getElementsByClassName("minus-none")[0].innerHTML= "₾594";
+  document.getElementsByClassName("number-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[1].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[2].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[4].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[5].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("infinity-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[3].style="box-shadow: 0px 2px #fff";
+}
+function myFunction5(){
+  document.getElementsByClassName("price-1")[0].innerHTML= "₾ 2,495";
+  document.getElementsByClassName("price-2")[0].style= "visibility:visible";
+  document.getElementsByClassName("price-2")[0].innerHTML= "-30%";
+  document.getElementsByClassName("price-3")[0].style="visibility:visible";
+  document.getElementsByClassName("price-3")[0].innerHTML= "ფასდაკლება";
+  document.getElementsByClassName("minus-price")[0].style= "visibility:visible";
+  document.getElementsByClassName("minus-price")[0].innerHTML= "₾3,564";
+  document.getElementsByClassName("minus-none")[0].innerHTML= "₾594";
+  document.getElementsByClassName("number-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[1].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[2].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[3].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[5].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("infinity-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[4].style="box-shadow: 0px 2px #fff";
+}
+function myFunction6(){
+  document.getElementsByClassName("price-1")[0].innerHTML= "₾ 3,564";
+  document.getElementsByClassName("price-2")[0].style= "visibility:visible";
+  document.getElementsByClassName("price-2")[0].innerHTML= "-40%";
+  document.getElementsByClassName("price-3")[0].style="visibility:visible";
+  document.getElementsByClassName("price-3")[0].innerHTML= "ფასდაკლება";
+  document.getElementsByClassName("minus-price")[0].style= "visibility:visible";
+  document.getElementsByClassName("minus-price")[0].innerHTML= "₾5,940";
+  document.getElementsByClassName("minus-none")[0].innerHTML= "₾594";
+  document.getElementsByClassName("number-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[1].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[2].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[3].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[4].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("infinity-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[5].style="box-shadow: 0px 2px #fff";
+}
+function myFunction7(){
+  document.getElementsByClassName("price-1")[0].innerHTML= "₾ 5940";
+  document.getElementsByClassName("price-2")[0].style= "visibility:hidden";
+  document.getElementsByClassName("price-2")[0].innerHTML= "-00%";
+  document.getElementsByClassName("price-3")[0].style="visibility:hidden";
+  document.getElementsByClassName("price-3")[0].innerHTML= "ფასდაკლება";
+  document.getElementsByClassName("minus-price")[0].style= "visibility:hidden";
+  document.getElementsByClassName("minus-none")[0].innerHTML= "₾594";
+  document.getElementsByClassName("number-icon")[0].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[1].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[2].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[3].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[4].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("number-icon")[5].style="box-shadow:0px 2px rgba(0,0,0,.075)";
+  document.getElementsByClassName("infinity-icon")[0].style="box-shadow: 0px 2px #fff";
+}
